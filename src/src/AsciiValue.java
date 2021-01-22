57deb41cad6050c82cb941efef5045d9080f5b24
chjayasree <chjayasree97@gmail.com>
2021-01-22 10:54:41 +0530

    add AsciiValue

---
+++ src/src/AsciiValue.java
@@ -0,0 +1,11 @@
+public class AsciiValue {
+    public static void main(String[] args){
+        int ch1='a';
+        int ch2='b';
+        int ch3='&';
+
+        System.out.println("the asciivalue of"+ch1);
+        System.out.println("the asciivalue of"+ch2);
+        System.out.println("the asciivalue of"+ch3);
+    }
+}
