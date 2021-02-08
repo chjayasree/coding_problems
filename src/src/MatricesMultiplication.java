b2ebc9b3600e5c7b8df43c5c47e688e320e56b8e
chjayasree <chjayasree97@gmail.com>
2021-02-08 13:19:18 +0530

    add MatricesMultiplication

---
+++ src/src/MatricesMultiplication.java
@@ -0,0 +1,18 @@
+public class MatricesMultiplication {
+    public static void main(String args[]){
+        int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int b[][]={{2,3,4},{5,6,7},{7,8,9}};
+        int c[][]=new int[3][3];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                c[i][j]=0;
+                for(int k=0;k<3;k++){
+                    c[i][j]=a[i][k]*b[k][j];
+                }
+                System.out.print(c[i][j]+" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
