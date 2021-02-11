0652b4e968f823bb801c66d402692ef55ffeda29
chjayasree <chjayasree97@gmail.com>
2021-02-11 11:38:47 +0530

    add LowerTriangularMatrix

---
+++ src/src/LowerTriangularMatrix.java
@@ -0,0 +1,25 @@
+public class LowerTriangularMatrix {
+    public static void main(String[] args){
+        int row,col;
+        int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+        row=a.length;
+        col=a[0].length;
+        if(row!=col){
+            System.out.println("Matrix should be a square matrix");
+        }
+        else{
+            for(int i=0;i<row;i++){
+                for(int j=0;j<col;j++){
+                    if(i>j){
+                        System.out.print("0");
+                    }
+                    else{
+                        System.out.print(a[i][j]+" ");
+                    }
+
+                }
+                System.out.println();
+            }
+        }
+    }
+}
